bac8daf84ff428270dceb690e01cac03a945712d
Guilherme Teixeira <guisebastian15@gmail.com>
2024-11-04 19:11:18 -0300

    Finish controller tests

--- src/test/java/com/tech/challenge/tech_challenge/adapters/driver/controllers/OrderControllerTest.java
+++ src/test/java/com/tech/challenge/tech_challenge/adapters/driver/controllers/OrderControllerTest.java
@@ -1,9 +1,10 @@
 package com.tech.challenge.tech_challenge.adapters.driver.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.tech.challenge.tech_challenge.core.domain.entities.EOrderStatus;
-import com.tech.challenge.tech_challenge.core.domain.entities.Order;
-import com.tech.challenge.tech_challenge.core.domain.entities.OrderBuilder;
+import com.tech.challenge.tech_challenge.core.application.exceptions.ResourceNotFoundException;
+import com.tech.challenge.tech_challenge.core.application.exceptions.ValidationException;
+import com.tech.challenge.tech_challenge.core.application.message.MessageResponse;
+import com.tech.challenge.tech_challenge.core.domain.entities.*;
 import com.tech.challenge.tech_challenge.core.domain.services.OrderService;
 import com.tech.challenge.tech_challenge.core.domain.services.extended.OrderClientService;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,9 +22,11 @@ import org.springframework.http.ResponseEntity;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -52,18 +55,19 @@ public class OrderControllerTest {
     @Test
     void listTest_Success() {
         Order order1 = new OrderBuilder()
-                .withOrderItems(null)
+                .withOrderItems(new ArrayList<>())
                 .withPayment(null)
                 .withStatus(EOrderStatus.ORDERING)
                 .build();
         Order order2 = new OrderBuilder()
-                .withOrderItems(null)
+                .withOrderItems(new ArrayList<>())
                 .withPayment(null)
                 .withStatus(EOrderStatus.ORDERING)
                 .build();
 
-        when(orderService.list()).thenReturn(List.of(order1,order2));
-        ResponseEntity<List> resultList = this.restTemplate.getForEntity(getFullUrl("/order"),
+        when(orderService.list()).thenReturn(List.of(order2,order1));
+
+        ResponseEntity<List> resultList  = this.restTemplate.getForEntity(getFullUrl("/order"),
                 List.class);
 
         List<Order> orders = new ArrayList<>();
@@ -75,6 +79,130 @@ public class OrderControllerTest {
         assertEquals(orders.size(),2);
         assertTrue(orders.contains(order1));
         assertTrue(orders.contains(order2));
+    }
+
+    @Test
+    void getByIdTest_Success() throws ResourceNotFoundException {
+        Order order = new OrderBuilder()
+                .withOrderItems(new ArrayList<>())
+                .withPayment(null)
+                .withStatus(EOrderStatus.ORDERING)
+                .build();
+
+        when(orderService.getById(order.getId())).thenReturn(order);
+        ResponseEntity<Order> result = this.restTemplate.getForEntity(getFullUrl("/order/" + order.getId()),
+                Order.class);
+
+        Order orderResult = result.getBody();
+
+        assertEquals(result.getStatusCode(), HttpStatus.OK);
+        assertEquals(order,orderResult);
+    }
+
+    @Test
+    void getByIdTest_NotFound() throws ResourceNotFoundException {
+        UUID id = UUID.randomUUID();
+
+        when(orderService.getById(id)).thenThrow(ResourceNotFoundException.class);
+        ResponseEntity<MessageResponse> result = this.restTemplate.getForEntity(getFullUrl("/order/" + id),
+                MessageResponse.class);
+
+        assertEquals(result.getStatusCode(), HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    void getByIdTest_BadRequest() throws ResourceNotFoundException {
+        String id = "7834687346";
+
+        ResponseEntity<MessageResponse> result = this.restTemplate.getForEntity(getFullUrl("/order/" + id),
+                MessageResponse.class);
+
+        assertEquals(result.getStatusCode(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    void createTest_Success() throws ValidationException {
+        Order order = new OrderBuilder().withId(null).build();
+        Order createdOrder = new OrderBuilder().build();
+
+        when(orderService.create(any())).thenReturn(createdOrder);
+
+        ResponseEntity<Order> result = this.restTemplate.postForEntity(
+                getFullUrl("/order"),
+                order,
+                Order.class
+        );
+
+        Order orderResult = result.getBody();
+
+        assertEquals(result.getStatusCode(), HttpStatus.CREATED);
+        assertEquals(createdOrder, orderResult);
+    }
+
+    @Test
+    void createTest_BadRequest() throws ValidationException {
+        Order order = new OrderBuilder().build();
+
+        when(orderService.create(any())).thenThrow(ValidationException.class);
+
+        ResponseEntity<MessageResponse> result = this.restTemplate.postForEntity(
+                getFullUrl("/order"),
+                order,
+                MessageResponse.class
+        );
+
+        assertEquals(result.getStatusCode(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    void addClientTest_Success() throws ValidationException, ResourceNotFoundException {
+        Client client = new ClientBuilder().build();
+        Order order = new OrderBuilder().withClient(client).build();
+
+        when(orderClientService.addClientToOrder(order.getId(), client.getId())).thenReturn(order);
+
+        ResponseEntity<Order> result = this.restTemplate.postForEntity(
+                getFullUrl(String.format("/order/%s/client/%s", order.getId(), client.getId())),
+                order,
+                Order.class
+        );
+
+        Order orderResult = result.getBody();
+
+        assertEquals(result.getStatusCode(), HttpStatus.OK);
+        assertEquals(order, orderResult);
+    }
+
+    @Test
+    void addClientTest_NotFound() throws ValidationException, ResourceNotFoundException {
+        Client client = new ClientBuilder().build();
+        Order order = new OrderBuilder().withClient(client).build();
+
+        when(orderClientService.addClientToOrder(order.getId(), client.getId())).thenThrow(ResourceNotFoundException.class);
+
+        ResponseEntity<MessageResponse> result = this.restTemplate.postForEntity(
+                getFullUrl(String.format("/order/%s/client/%s", order.getId(), client.getId())),
+                order,
+                MessageResponse.class
+        );
+
+        assertEquals(result.getStatusCode(), HttpStatus.NOT_FOUND);
+    }
+
+    @Test
+    void addClientTest_BadRequest() throws ValidationException, ResourceNotFoundException {
+        Client client = new ClientBuilder().build();
+        Order order = new OrderBuilder().withClient(client).build();
+
+        when(orderClientService.addClientToOrder(order.getId(), client.getId())).thenThrow(ValidationException.class);
+
+        ResponseEntity<MessageResponse> result = this.restTemplate.postForEntity(
+                getFullUrl(String.format("/order/%s/client/%s", order.getId(), client.getId())),
+                order,
+                MessageResponse.class
+        );
+
+        assertEquals(result.getStatusCode(), HttpStatus.BAD_REQUEST);
     }
 
     private String getFullUrl(String endpoint){
--- src/test/java/com/tech/challenge/tech_challenge/core/domain/entities/OrderBuilder.java
+++ src/test/java/com/tech/challenge/tech_challenge/core/domain/entities/OrderBuilder.java
@@ -1,9 +1,6 @@
 package com.tech.challenge.tech_challenge.core.domain.entities;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 
 
 public class OrderBuilder {
@@ -12,10 +9,13 @@ public class OrderBuilder {
     private Payment payment;
     private List<OrderItem> orderItems;
     private EOrderStatus status;
+    private Client client;
 
     public OrderBuilder(){
         id = UUID.randomUUID();
         status = EOrderStatus.PAYMENT_PENDING;
+
+        client = null;
 
         payment = new Payment();
         payment.setId(UUID.randomUUID());
@@ -51,6 +51,11 @@ public class OrderBuilder {
         return this;
     }
 
+    public OrderBuilder withClient(Client client){
+        this.client = client;
+        return this;
+    }
+
     public OrderBuilder withOrderItem(OrderItem orderItem){
         this.orderItems = new ArrayList<>();
         orderItems.add(orderItem);
@@ -72,6 +77,8 @@ public class OrderBuilder {
         order.setId(id);
         order.setPayment(payment);
         order.setStatus(status);
+        order.setOrderItems(new HashSet<>());
+        order.setClient(client);
 
         if(Objects.nonNull(orderItems)){
             for(OrderItem orderItem : orderItems){
