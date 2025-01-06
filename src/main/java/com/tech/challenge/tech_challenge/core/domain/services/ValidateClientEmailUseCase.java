f3c4173166d86c4a64599ab03a0ef5f2a0254872
First Last <you@email.com>
2025-01-05 21:20:33 -0300

    Updated validation logic. Emails without @ were able to be inserted with old logic

--- src/main/java/com/tech/challenge/tech_challenge/core/domain/services/ValidateClientEmailUseCase.java
+++ src/main/java/com/tech/challenge/tech_challenge/core/domain/services/ValidateClientEmailUseCase.java
@@ -13,7 +13,7 @@ public class ValidateClientEmailUseCase {
 
     public static void validate(String email) throws ValidationException {
 
-        if (StringUtils.isEmpty(email) && !EmailValidator.isValidEmail(email)) throw new InvalidEmailAddress(email);
+        if (StringUtils.isEmpty(email) || !EmailValidator.isValidEmail(email)) throw new InvalidEmailAddress(email);
 
     }
 }
