7bd8d307373568c7881a8dba442909b01240e4f5
First Last <you@email.com>
2025-01-16 21:22:09 -0300

    Refactor AddPaymentToOrderUseCaseTest

--- src/test/java/com/tech/challenge/tech_challenge/core/domain/useCases/AddPaymentToOrderUseCaseTest.java
+++ src/test/java/com/tech/challenge/tech_challenge/core/domain/useCases/AddPaymentToOrderUseCaseTest.java
@@ -3,6 +3,7 @@ package com.tech.challenge.tech_challenge.core.domain.useCases;
 import com.tech.challenge.tech_challenge.adapters.driven.infra.repositories.OrderRepository;
 import com.tech.challenge.tech_challenge.core.domain.entities.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -20,7 +21,7 @@ public class AddPaymentToOrderUseCaseTest {
     @MockBean
     private CreatePaymentUseCase createPaymentUseCase;
 
-    @MockBean
+    @Autowired
     private AddPaymentToOrderUseCase  addPaymentToOrderUseCase;
 
     @Test
