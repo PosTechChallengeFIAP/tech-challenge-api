6c76efa9acc2396aad85c85f4988ce75d09d3c39
GregoriSimei <gregorisimei@gmail.com>
2025-01-18 00:28:03 -0300

    refactor: adjust mappers

--- src/main/java/com/tech/challenge/tech_challenge/adapters/driven/infra/repositories/mappers/PaymentEntityMapper.java
+++ src/main/java/com/tech/challenge/tech_challenge/adapters/driven/infra/repositories/mappers/PaymentEntityMapper.java
@@ -6,11 +6,19 @@ import com.tech.challenge.tech_challenge.core.domain.entities.Payment;
 public class PaymentEntityMapper {
     public static Payment toDomain(PaymentEntity paymentEntity) {
         Payment payment = new Payment();
+        payment.setId(paymentEntity.getId());
+        payment.setValue(paymentEntity.getValue());
+        payment.setStatus(paymentEntity.getStatus());
+        payment.setPaymentURL(paymentEntity.getPaymentURL());
         return payment;
     }
 
     public static PaymentEntity toEntity(Payment payment) {
         PaymentEntity paymentEntity = new PaymentEntity();
+        paymentEntity.setId(payment.getId());
+        paymentEntity.setValue(payment.getValue());
+        paymentEntity.setStatus(payment.getStatus());
+        paymentEntity.setPaymentURL(payment.getPaymentURL());
         return paymentEntity;
     }
 }
--- src/main/java/com/tech/challenge/tech_challenge/adapters/driven/infra/repositories/mappers/ProductEntityMapper.java
+++ src/main/java/com/tech/challenge/tech_challenge/adapters/driven/infra/repositories/mappers/ProductEntityMapper.java
@@ -6,11 +6,23 @@ import com.tech.challenge.tech_challenge.core.domain.entities.Product;
 public class ProductEntityMapper {
     public static Product toDomain(ProductEntity productEntity) {
         Product product = new Product();
+        product.setId(productEntity.getId());
+        product.setName(productEntity.getName());
+        product.setDescription(productEntity.getDescription());
+        product.setCategory(productEntity.getCategory());
+        product.setPrice(productEntity.getPrice());
+        product.setActive(productEntity.getActive());
         return product;
     }
 
     public static ProductEntity toEntity(Product product) {
         ProductEntity productEntity = new ProductEntity();
+        productEntity.setId(product.getId());
+        productEntity.setName(product.getName());
+        productEntity.setDescription(product.getDescription());
+        productEntity.setCategory(product.getCategory());
+        productEntity.setPrice(product.getPrice());
+        productEntity.setActive(product.getActive());
         return productEntity;
     }
 }
