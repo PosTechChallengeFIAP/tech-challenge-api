6ae4c2f55188e5ca6a62de103e2eecdeb87a44a0
GregoriSimei <gregorisimei@gmail.com>
2025-03-21 03:29:13 -0300

    ci: remove alb health check

--- devops/terraform/alb.tf
+++ devops/terraform/alb.tf
@@ -18,13 +18,13 @@ resource "aws_lb_target_group" "ecs_target_group" {
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.network.outputs.main_vpc_id
 
-  health_check {
-    interval            = 30
-    path                = "/health"
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
+  # health_check {
+  #   interval            = 30
+  #   path                = "/health"
+  #   timeout             = 5
+  #   healthy_threshold   = 2
+  #   unhealthy_threshold = 2
+  # }
 }
 
 resource "aws_lb_listener" "ecs_listener" {
