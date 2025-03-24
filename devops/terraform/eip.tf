a52537f3d0802fde6a29a1b24d55f4f5535f9cf7
GregoriSimei <gregorisimei@gmail.com>
2025-03-24 02:40:05 -0300

    ci: test

---
+++ devops/terraform/eip.tf
@@ -0,0 +1,30 @@
+resource "aws_eip" "ecs_instance_eip" {
+  instance = aws_instance.ecs_instance.id
+  domain   = "vpc"
+  tags = {
+    Name = "tech-challenge-api-ecs-instance-eip"
+  }
+}
+
+resource "aws_nat_gateway" "ecs_nat_gateway" {
+  allocation_id = aws_eip.ecs_instance_eip.id
+  subnet_id     = data.terraform_remote_state.network.outputs.api_public_subnet_id
+
+  tags = {
+    Name = "tech-challenge-nat-gateway"
+  }
+}
+
+resource "aws_route" "private_subnet_nat_route" {
+  route_table_id         = aws_route_table.private_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.ecs_nat_gateway.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = data.terraform_remote_state.network.outputs.main_vpc_id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
